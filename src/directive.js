e78de5bc3162b65eb75379f4297ab50242c73d0b
shrpne <shrpne@gmail.com>
2019-04-08 19:46:38 +0300

    fix initial resize

--- src/directive.js
+++ src/directive.js
@@ -3,12 +3,16 @@ import autosize from 'autosize';
 export default {
     bind(el, binding) {
         if (el.tagName === 'TEXTAREA') {
-            autosize(el);
+            this.$nextTick(() => {
+                autosize(el);
+            });
         }
     },
     componentUpdated(el, binding, vnode) {
         if (el.tagName === 'TEXTAREA') {
-            autosize.update(el);
+            this.$nextTick(() => {
+                autosize.update(el);
+            });
         }
     },
     unbind(el) {
